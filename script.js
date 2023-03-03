9740daa8000b67adb92f0c0ef7a5d6d4c383de9c
AYoung9867 <102661297+AYoung9867@users.noreply.github.com>
2023-03-03 14:08:43 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 //carousel
 
+setInterval(slideChange,5000);
+
+function slideChange() {
+	const slides = document.querySelector("[data-slides]")
+	const activeSlide = slides.querySelector("[data-active]")
+	let newIndex = [...slides.children].indexOf(activeSlide) + 1
+	if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+}
+
 const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
